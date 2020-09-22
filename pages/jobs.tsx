f2e2bf80fc835fcb64632d4bbef3e61ce99ca731
Mariano D'Agostino <mdagostino@digitalhouse.com>
2020-09-22 19:32:49 -0300

    Typeform test

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import PageContainer from "../components/pageContainer/PageContainer";
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+import * as typeformEmbed from '@typeform/embed';
 
 export default function Jobs() {
+  const typeform = typeformEmbed.makePopup("https://form.typeform.com/to/OAJqKlIH", {});
+
   return (
     <>
       <h1>Jobs</h1>
+      <div className="ExamplePopup">
+        {/*<ReactTypeformEmbed*/}
+        {/*  popup*/}
+        {/*  autoOpen={false}*/}
+        {/*  url="https://form.typeform.com/to/OAJqKlIH"*/}
+        {/*  hideHeaders*/}
+        {/*  hideFooter*/}
+        {/*  buttonText="Go!"*/}
+        {/*  style={{ top: 100 }}*/}
+        {/*/>*/}
+        <button className="btn" onClick={() => {typeform.open()}} style={{ cursor: 'pointer' }}>
+          Click to open the popup!
+        </button>
+      </div>
     </>
   );
 }
