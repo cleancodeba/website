09f67f39545cb8e39a21453e6875883ced980583
Mariano D'Agostino <mdagostino@digitalhouse.com>
2020-09-22 21:38:04 -0300

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import { AppProps } from 'next/app'
 import NavigationBar from "../components/navbar/NavigationBar";
 import Footer from "../components/footer/Footer";
 import PageContainer from "../components/pageContainer/PageContainer";
+import {Router} from "next/router";
+import {GTMPageView} from "../components/utils/gtm";
+import {useEffect} from "react";
 
 export function CleanCodeApp({ Component, pageProps }: AppProps) {
+
+  // Initiate GTM
+  useEffect(() => {
+    const handleRouteChange = (url: string) => GTMPageView(url);
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (<>
     <NavigationBar />
     <PageContainer>
